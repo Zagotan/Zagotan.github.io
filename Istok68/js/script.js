bb1a57f2efb491187f475d00d01dd508d16d660f
Zagotan <77735802+Zagotan@users.noreply.github.com>
2021-02-18 19:26:33 +0300

    Создал папку с проектом
    Закачал файлы на репозиторий

---
+++ Istok68/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//Плавная прокрутка	к якорю
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});
